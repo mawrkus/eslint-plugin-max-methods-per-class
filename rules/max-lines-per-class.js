f22658815f3f346f105debea909016ed75eb6ec6
mawrkus <web@sparring-partner.be>
2021-08-19 00:11:05 +0200

    The origin of time..

---
+++ rules/max-lines-per-class.js
@@ -0,0 +1,67 @@
+/**
+ * @fileoverview Enforce a maximum number of methods per class
+ * @author mawrkus <web@sparring-partner.be>
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+const DEFAULT_MAX_COUNT = 5;
+
+module.exports = {
+  meta: {
+    type: "suggestion",
+    docs: {
+      description: "enforce a maximum number of methods per class",
+      category: "Best Practices",
+      recommended: false,
+      url: "https://github.com/mawrkus/eslint-plugin-max-methods-per-class",
+    },
+    schema: [
+      {
+        type: "integer",
+        minimum: 0,
+        default: DEFAULT_MAX_COUNT,
+      },
+    ],
+    messages: {
+      maximumExceeded:
+        "Class has too many methods ({{ methodsCount }}). Maximum allowed is {{ maximumCount }}.",
+    },
+  },
+  create(context) {
+    const maximumCount = context.options[0] || 1;
+
+    let methodsCount = 0;
+
+    const checkClassMethodsCount = (node) => {
+      if (methodsCount > maximumCount) {
+        context.report({
+          node,
+          messageId: "maximumExceeded",
+          data: {
+            methodsCount,
+            maximumCount,
+          },
+        });
+      }
+    };
+
+    return {
+      "ClassDeclaration, ClassExpression"() {
+        methodsCount = 0;
+      },
+      MethodDefinition() {
+        methodsCount += 1;
+      },
+      "ClassDeclaration:exit": checkClassMethodsCount,
+      "ClassExpression:exit": checkClassMethodsCount,
+    };
+  },
+};
---
+++ tests/max-lines-per-class.js
@@ -0,0 +1,57 @@
+"use strict";
+
+const rule = require("../rules/max-lines-per-class");
+const { RuleTester } = require("eslint");
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 6 } });
+
+const code1 = `
+class Rule1 {
+  constructor() {}
+  [Symbol.iterator]() {}
+  get prop() {}
+  method1() {}
+  static method2() {}
+}
+`;
+
+const code2 = `
+class Rule3 {
+  constructor() {}
+  method() {}
+}
+
+class Rule4 {
+  constructor() {}
+  method1() {}
+  method2() {}
+}
+`;
+
+ruleTester.run("max-lines-per-class", rule, {
+  valid: [
+    {
+      code: code1,
+      options: [5],
+    },
+    {
+      code: code2,
+      options: [3],
+    },
+  ],
+  invalid: [
+    {
+      code: code2,
+      options: [1],
+      errors: [
+        { message: "Class has too many methods (2). Maximum allowed is 1." },
+        { message: "Class has too many methods (3). Maximum allowed is 1." },
+      ],
+    },
+    {
+      code: code2,
+      options: [2],
+      errors: [{ message: "Class has too many methods (3). Maximum allowed is 2." }],
+    },
+  ],
+});
